ad5404567cc5b9d712f2ee995314a890be4d0060
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-05-10 17:15:37 +0700

    test

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@
         ToyStore toyStore = new ToyStore();
         toyStore.addNewToy(1, "Doll", 10, 20);
         toyStore.addNewToy(2, "Car", 15, 30);
-        toyStore.addNewToy(3, "Teddy Bear", 12, 25);
-        toyStore.addNewToy(4, "Puzzle", 20, 25);
+        toyStore.addNewToy(3, "dog", 12, 25);
+        toyStore.addNewToy(4, "cat", 20, 25);
 
         toyStore.changeFrequency(2, 10);
 
